a589e877feda6ecd7c57dc438adf2756641d728d
AbhayShastry <abhay.s.shastry@gmail.com>
2021-10-30 21:31:54 +0530

    Added a few comments

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+
+import react, { Component } from 'react';
+import { Button } from 'react-native';
 import './App.css';
+import Sentiment from 'sentiment';
+const sentiment = new Sentiment();
+//This is a Comment 
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  constructor(props){
+    super(props);
+    this.state={
+        sentimentScore: null,
+        generalSentiment: null
+    };
+    this.findSentiment = this.findSentiment.bind(this);
+  }
 
-export default App;
+  findSentiment(event) {
+    const result = sentiment.analyze(event.target.value)
+    //console.log(result)
+    this.setState({
+      sentimentScore: result.score
+    })
+    if(result.score < 0 && result.score >= -3){
+      this.setState({generalSentiment: 'Negative'})
+    }
+    else if(result.score < -3){
+      this.setState({generalSentiment: 'Extremely Negative'})
+    }
+    else if(result.score > 0 && result.score <= 3){
+      this.setState({generalSentiment: 'Positive'})
+    }
+    else if(result.score > 3){
+      this.setState({generalSentiment: 'Extrememly Positive'})
+    }
+    else{
+      this.setState({generalSentiment: 'No sentiment'})
+    }
+    
+  }
+  
+  
+
+  render() {
+    return(
+      <div className="App">
+       <h2> Sentiment Analyser in React </h2>
+       <p>Enter Text for Analysis</p>
+       <textarea onChange={this.findSentiment} />
+       <Button title='Find Sentiment' onClick ={this.onClick}/>
+       <p>Sentiment Score: <textfield> {this.state.sentimentScore}</textfield></p>
+       <p>General Sentiment: <textfield>{this.state.generalSentiment}</textfield></p>
+     </div> 
+
+    )
+  }  
+ }
+
+export default App; 
